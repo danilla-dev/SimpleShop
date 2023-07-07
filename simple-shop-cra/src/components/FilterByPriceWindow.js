a133edd482f5c4dcdddd5a07bbe12f1d316b2342
DanielKam <dkaminski38p@protonmail.com>
2023-07-07 18:31:36 +0200

    Funcitonality of input text - user can input only numbers and when min value is biggest than max user have a error and cant filter products

--- simple-shop-cra/src/components/FilterByPriceWindow.js
+++ simple-shop-cra/src/components/FilterByPriceWindow.js
@@ -6,13 +6,13 @@ import "../styles/FilterByPriceWindow.scss";
 
 const FilterByPriceWindow = () => {
   const { filterByPrice } = useContext(ProductsContext);
-  const [minPrice, setMinPrice] = useState("1");
+  const [minPrice, setMinPrice] = useState("");
   const [maxPrice, setMaxPrice] = useState("");
   const [priceRange, setPriceRange] = useState({ min: 1, max: 2 });
+  const [isCorrectRange, setIsCorrectRange] = useState(true);
 
   const handleValueChange = ({ target, keyCode }) => {
-    const value = target.value.split(" ").join("");
-
+    const value = target.value.replace(/\s/g, "");
     if (value >= 0) {
       if (target.name === "min") {
         setMinPrice(value);
@@ -28,27 +28,50 @@ const FilterByPriceWindow = () => {
       }
     }
   };
+  const handleSetPrice = (e) => {
+    e.preventDefault();
+    const min = parseInt(minPrice);
+    const max = parseInt(maxPrice);
+    if (min > max) {
+      console.log("zla wartosc od do ");
+      setIsCorrectRange(false);
+    } else {
+      setPriceRange({
+        min,
+        max,
+      });
+      setIsCorrectRange(true);
+    }
+  };
 
   return (
     <div className="sort-price">
-      <p>Sort by max price: </p>
+      <p>Sort by price: </p>
       <form>
-        <input
-          className="sort-price_min-value"
-          name="min"
-          type="text"
-          onChange={handleValueChange}
-          onKeyDown={handleValueChange}
-          value={minPrice}
-        />
-        <input
-          className="sort-price_max-value"
-          name="max"
-          type="text"
-          onChange={handleValueChange}
-          onKeyDown={handleValueChange}
-          value={maxPrice}
-        />
+        <label htmlFor="min">
+          Min:
+          <input
+            className="sort-price_min-value"
+            name="min"
+            type="text"
+            onChange={handleValueChange}
+            onKeyDown={handleValueChange}
+            value={minPrice}
+          />
+        </label>
+        <label htmlFor="max">
+          Max:
+          <input
+            className="sort-price_max-value"
+            name="max"
+            type="text"
+            onChange={handleValueChange}
+            onKeyDown={handleValueChange}
+            value={maxPrice}
+          />
+          {!isCorrectRange && <p>Incorrect value of price</p>}
+        </label>
+        <button onClick={handleSetPrice}>Set price</button>
       </form>
     </div>
   );
