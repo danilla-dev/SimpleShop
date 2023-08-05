08541a9c6e349a4148b74c7b6de78569630eae66
DanielKam <dkaminski38p@protonmail.com>
2023-08-05 18:11:05 +0200

    New display of categories on home page

--- simple-shop-cra/src/components/HomePageCategories.js
+++ simple-shop-cra/src/components/HomePageCategories.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+import { useMediaQuery } from "react-responsive";
+
 const HomePageCategories = () => {
+  const isTablet = useMediaQuery({ query: "(min-width: 768px)" });
+
   let categoriesDescriptions = [
     {
       name: "Guns",
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, explicabo. Similique, tempora? Optio commodi quo magni quae, nisi voluptatum saepe! Qui voluptatum magni mollitia quibusdam officiis ab magnam suscipit enim!",
       img: "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/ray-guyn-sm.jpg",
+      imgLG:
+        "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/ray-guyn-lg.jpg",
       path: "/products/Guns",
     },
     {
@@ -14,6 +21,8 @@ const HomePageCategories = () => {
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, explicabo. Similique, tempora? Optio commodi quo magni quae, nisi voluptatum saepe! Qui voluptatum magni mollitia quibusdam officiis ab magnam suscipit enim!",
       img: "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/portal-sm.jpg",
+      imgLG:
+        "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/portal-lg.jpg",
       path: "/products/Special",
     },
     {
@@ -21,25 +30,25 @@ const HomePageCategories = () => {
       description:
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, explicabo. Similique, tempora? Optio commodi quo magni quae, nisi voluptatum saepe! Qui voluptatum magni mollitia quibusdam officiis ab magnam suscipit enim!",
       img: "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/plumbus-sm.jpg",
+      imgLG:
+        "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/plumbus-lg.jpg",
       path: "/products/Other",
     },
   ];
 
   categoriesDescriptions = categoriesDescriptions.map((desc) => {
-    const { name, description, img, path } = desc;
+    const { name, description, img, imgLG, path } = desc;
     return (
-      <div
-        className={`categories-description categories-descriptions__${name}`}
-      >
-        <div className="categories-descriptions__image">
-          <img src={img} alt="" />
+      <div className={`categories-description categories-description__${name}`}>
+        <div className="categories-description__image">
+          <img src={isTablet ? imgLG : img} alt="" />
         </div>
-        <div className="categories-descriptions__info">
-          <h3 className={`categories-descriptions__info-name`}>{name}</h3>
-          <p className={`categories-descriptions__info-description`}>
+        <div className="categories-description__info">
+          <h3 className={`categories-description__info-name`}>{name}</h3>
+          {/* <p className={`categories-descriptions__info-description`}>
             {description}
-          </p>
-          <div className="categories-descriptions__link">
+          </p> */}
+          <div className="categories-description__link">
             <Link to={path}>GO!</Link>
           </div>
         </div>
