01d10b246cbc0c6f5194906a50ceefc5fe5815a9
DanielKam <dkaminski38p@protonmail.com>
2023-08-05 22:07:54 +0200

    Create a carousel to most popular section with React Responsive Carousel

--- simple-shop-cra/src/components/MostPopularProducts.js
+++ simple-shop-cra/src/components/MostPopularProducts.js
@@ -1,9 +1,55 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { useMediaQuery } from "react-responsive";
+import { GiAlienFire } from "react-icons/gi";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import "../styles/MostPopularProducts.scss";
+
+import { popularProducts } from "../db/PopularProducts";
+
 const MostPopularProducts = () => {
+  const isDesktop = useMediaQuery({ query: "(min-width: 768px)" });
+
+  const products = popularProducts.map((product, index) => {
+    return (
+      <li key={index}>
+        <div className={"product" + " " + product.title}>
+          <div className="product-image">
+            <img
+              src={isDesktop ? product.imageDesk : product.imageMobile}
+              alt=""
+            />
+          </div>
+          <div className="product-info">
+            <h3 className="product__name">{product.title}</h3>
+            <p className="product__category">{product.category}</p>
+            <div className="product__price">
+              <span>{product.price}</span>
+              <GiAlienFire />
+            </div>
+            <div className="product__action-buttons">
+              <button className="product__action-buttons-add">
+                <FontAwesomeIcon className="cart-icon" icon={faCartPlus} />
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+    );
+  });
   return (
-    <>
-      <></>
-    </>
+    <div className="most-popular-products__background">
+      <div className="most-popular-products wrapper">
+        <div className="most-popular-products__carousel">
+          <Carousel autoPlay="true" infiniteLoop="true">
+            {products}
+          </Carousel>
+        </div>
+      </div>
+    </div>
   );
 };
 
---
+++ simple-shop-cra/src/db/PopularProducts.js
@@ -0,0 +1,38 @@
+export const popularProducts = [
+  {
+    title: "Portal gun",
+    category: "Special",
+    price: 320,
+    description:
+      "Unlock the wonders of the multiverse with the Portal Gun! Create instant portals to traverse dimensions and embark on thrilling adventures. Inspired by Rick's ingenuity, this gadget lets you bend the rules of space and time effortlessly. Aim, shoot, and prepare for mind-bending exploration!",
+    imageMobile:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/portal-sm.jpg",
+    imageDesk:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/portal-lg.jpg",
+    path: "",
+  },
+  {
+    title: "Plumbus",
+    category: "Other",
+    price: 20,
+    description:
+      "Introducing the Plumbus - the must-have item for every Rick and Morty enthusiast! Crafted with unrivaled ingenuity and a touch of Blamf, the Plumbus is a versatile tool that defies explanation. Want to Fleeb your Gumbles? The Plumbus has got you covered. Need to Schleem your Dinglebop? Look no further! It's an essential accessory for any Rick's arsenal. Grab your Plumbus today and discover a universe of uses that'll leave everyone else in awe. Don't be a Glip-Glop – get your Plumbus now!",
+    imageMobile:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/plumbus-sm.jpg",
+    imageDesk:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/plumbus-lg.jpg",
+    path: "",
+  },
+  {
+    title: "Ray gun",
+    category: "Guns",
+    price: 200,
+    description:
+      'Unleash the power of the "Ray Gun" and become an interdimensional warrior to be reckoned with! This cutting-edge laser weapon is designed to neutralize adversaries from various dimensions. Its advanced laser technology delivers swift and precise firepower against any interdimensional threat you may encounter. Equip yourself with the extraordinary "Ray Gun" and conquer the challenges of the multiverse in style!',
+    imageMobile:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/ray-guyn-sm.jpg",
+    imageDesk:
+      "https://res.cloudinary.com/di3axjqgh/image/upload/v1687886641/InterStore/ray-guyn-lg.jpg",
+    path: "",
+  },
+];
