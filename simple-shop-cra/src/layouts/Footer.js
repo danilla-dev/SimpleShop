8a9f2cfd4a4afa6cd0120d953b2f402d565aae7e
DanielKam <dkaminski38p@protonmail.com>
2023-08-06 17:27:43 +0200

    create git pages

--- simple-shop-cra/src/layouts/Footer.js
+++ simple-shop-cra/src/layouts/Footer.js
@@ -2,30 +2,35 @@ import React from "react";
 import "../styles/Footer.scss";
 
 import { PiSpiralFill } from "react-icons/pi";
+import "../styles/Footer.scss";
 
 const Footer = () => {
   return (
-    <div className="footer__background">
-      <div className="footer wrapper">
-        <div className="nav__logo">
-          <PiSpiralFill className="nav__logo-icon" />
-          <h1 className="nav__logo-text">
+    <>
+      <div className="contact-us__background">
+        <div className="contact-us wrapper">
+          <div className="nav__logo">
+            <PiSpiralFill className="nav__logo-icon" />
             <span>InterStore</span>
-          </h1>
-        </div>
-        <div className="footer__contact-form">
-          <form>
-            <label htmlFor="email">Your email:</label>
-            <input id="email" type="text" />
-            <label htmlFor="subject">Subject of message</label>
-            <input id="subject" type="text" />
-            <label htmlFor="text">Your messsage:</label>
-            <textarea name="text" id="text" cols="30" rows="10"></textarea>
-            <button></button>
-          </form>
+          </div>
+          <div className="contact-us__contact-form">
+            <div className="contact-us__header">Contact us.</div>
+            <form>
+              <label htmlFor="email">Your email:</label>
+              <input id="email" type="text" />
+              <label htmlFor="subject">Subject of message</label>
+              <input id="subject" type="text" />
+              <label htmlFor="text">Your messsage:</label>
+              <textarea name="text" id="text" cols="30" rows="5"></textarea>
+              <button className="footer-form__button">Send question</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="author-description">
+        Created by <a href="https://github.com/Danilla-dev">Danilla-dev</a>
+      </div>
+    </>
   );
 };
 
